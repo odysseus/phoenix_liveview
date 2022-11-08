5575f16b3591e00b7cfc5b9e788152f2d23cfa4f
Ryan Case <rmcase@gmail.com>
2022-11-08 16:53:11 -0700

    Added Catalog context and Product resource

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -22,9 +22,6 @@ defmodule PentoWeb.Router do
 
     get "/", PageController, :index
 
-    live_session :default, on_mount: PentoWeb.UserAuthLive do
-      live "/guess", WrongLive
-    end
   end
 
   # Other scopes may use custom stacks.
@@ -82,6 +79,17 @@ defmodule PentoWeb.Router do
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+
+    live_session :default, on_mount: PentoWeb.UserAuthLive do
+      live "/guess", WrongLive
+
+      live "/products", ProductLive.Index, :index
+      live "/products/new", ProductLive.Index, :new
+      live "/products/:id/edit", ProductLive.Index, :edit
+
+      live "/products/:id", ProductLive.Show, :show
+      live "products/:id/show/edit", ProductLive.Show, :edit
+    end
   end
 
   scope "/", PentoWeb do
