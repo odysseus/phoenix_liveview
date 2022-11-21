6d84fe71c55a1dcdbc3cdc94f7563b5fa1320ba3
Ryan Case <rmcase@gmail.com>
2022-11-21 13:42:42 -0700

    Fixed a route missing a leading slash

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -87,7 +87,7 @@ defmodule PentoWeb.Router do
       live "/products/:id/edit", ProductLive.Index, :edit
 
       live "/products/:id", ProductLive.Show, :show
-      live "products/:id/show/edit", ProductLive.Show, :edit
+      live "/products/:id/show/edit", ProductLive.Show, :edit
     end
   end
 
