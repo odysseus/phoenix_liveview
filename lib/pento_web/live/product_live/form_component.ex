3817ee5d56f43519d1d49e124d5a0cd12f90d6f5
Ryan Case <rmcase@gmail.com>
2022-11-30 15:31:26 -0700

    Added photo uploads to products

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -10,7 +10,14 @@ defmodule PentoWeb.ProductLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image,
+       accept: ~w(.jpg .jpeg .png),
+       max_entries: 1,
+       max_file_size: 9_000_000,
+       auto_upload: true,
+       progress: &handle_progress/3
+     )}
   end
 
   @impl true
@@ -27,8 +34,14 @@ defmodule PentoWeb.ProductLive.FormComponent do
     save_product(socket, socket.assigns.action, product_params)
   end
 
-  defp save_product(socket, :edit, product_params) do
-    case Catalog.update_product(socket.assigns.product, product_params) do
+  defp save_product(socket, :edit, params) do
+    result =
+      Catalog.update_product(
+        socket.assigns.product,
+        product_params(socket, params)
+      )
+
+    case result do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -40,8 +53,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
     end
   end
 
-  defp save_product(socket, :new, product_params) do
-    case Catalog.create_product(product_params) do
+  defp save_product(socket, :new, params) do
+    case Catalog.create_product(product_params(socket, params)) do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -51,5 +64,35 @@ defmodule PentoWeb.ProductLive.FormComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp product_params(socket, params) do
+    Map.put(params, "image_upload", socket.assigns.image_upload)
+  end
+
+  defp handle_progress(:image, entry, socket) do
+    if entry.done? do
+      {:ok, path} =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+
+      {:noreply,
+       socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> assign(:image_upload, path)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  defp upload_static_file(%{path: path}, socket) do
+    dest = Path.join("priv/static/uploads", Path.basename(path))
+    File.cp!(path, dest)
+    path = Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+
+    {:ok, {:ok, path}}
   end
 end
