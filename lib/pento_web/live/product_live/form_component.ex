65d0b458bcbe85192e5371d3798101aad9bb173e
Ryan Case <rmcase@gmail.com>
2022-12-01 12:40:00 -0700

    UX improvements for uploads.

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -71,12 +71,15 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   defp handle_progress(:image, entry, socket) do
+    :timer.sleep(500)
+
     if entry.done? do
       {:ok, path} =
         consume_uploaded_entry(
           socket,
           entry,
           &upload_static_file(&1, socket)
+          # &upload_image_file(Map.put(&1, :type, entry.client_type), socket)
         )
 
       {:noreply,
@@ -91,8 +94,41 @@ defmodule PentoWeb.ProductLive.FormComponent do
   defp upload_static_file(%{path: path}, socket) do
     dest = Path.join("priv/static/uploads", Path.basename(path))
     File.cp!(path, dest)
-    path = Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+    {:ok, {:ok, Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")}}
+  end
 
-    {:ok, {:ok, path}}
+  defp upload_image_file(%{path: path, type: ctype}, socket) do
+    ext =
+      case ctype do
+        "image/jpeg" -> ".jpg"
+        "image/png" -> ".png"
+      end
+
+    dest = Path.join("priv/static/uploads", Path.basename(path) <> ext)
+    File.cp!(path, dest)
+    route = Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+    dbg()
+
+    {:ok, {:ok, route}}
+  end
+
+  def upload_image_error(%{image: %{errors: errors}}, entry)
+      when length(errors) > 0 do
+    {_, msg} =
+      Enum.find(errors, fn {ref, _} ->
+        ref == entry.ref || ref == entry.upload_ref
+      end)
+
+    upload_error_msg(msg)
+  end
+
+  def upload_image_error(_, _), do: ""
+
+  def upload_error_msg(msg) do
+    case msg do
+      :not_accepted -> "Invalid file type"
+      :too_many_files -> "Too many files"
+      :too_large -> "File is too large"
+    end
   end
 end
