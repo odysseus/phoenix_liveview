c45c4f64176ff003c0dd5801246d613bb351b8c0
Ryan Case <rmcase@gmail.com>
2022-12-16 08:12:57 -0700

    Added event handling to user tracking.

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,13 +1,16 @@
 defmodule PentoWeb.Admin.DashboardLive do
   use PentoWeb, :live_view
+  alias PentoWeb.Admin.UserActivityLive
   alias PentoWeb.Admin.SurveyResultsLive
   alias PentoWeb.Endpoint
 
   @survey_results_topic "survey_results"
+  @user_activity_topic "user_activity"
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
       Endpoint.subscribe(@survey_results_topic)
+      Endpoint.subscribe(@user_activity_topic)
     end
 
     {:ok,
@@ -20,6 +23,15 @@ defmodule PentoWeb.Admin.DashboardLive do
     send_update(
       SurveyResultsLive,
       id: socket.assigns.survey_results_component_id
+    )
+
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id
     )
 
     {:noreply, socket}
